5378012caba2611fcbbf6aea90453ee794aab109
Ibrahim Katari <elsaid@live.utm.my>
2021-04-18 09:47:23 +0200

    adding right dependency

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const cors = require('cors')
 // const hpp = require('hpp');
-
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -21,6 +21,8 @@ const app = express();
 // 1) GLOBAL MIDDLEWARES
 // Set security HTTP headers
 app.use(helmet());
+app.use(cors());
+
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
