95e660c8a5d59d65d37da5f61d093d18237cd353
Ibrahim Katari <elsaid@live.utm.my>
2021-06-11 22:48:17 +0200

    adding the attrs

---
+++ models/semsterModel_copy.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const semsterSchema = new mongoose.Schema({
+  RegistrationStart: {
+    type: Date,
+    required: [true, '']
+  },
+  RegistrationEnd: {
+    type: Date,
+    required: [true, '']
+  },
+  ClassStart: {
+    type: Date,
+    required: [true, '']
+  },
+  ClassEnd: {
+    type: Date,
+    required: [true, '']
+  },
+  ModaifactionPeriodStart: {
+    type: Date,
+    required: [true, '']
+  },
+  ModaifactionPeriodEnd: {
+    type: Date,
+    required: [true, '']
+  },
+  Seeison: {
+    type: String,
+    required: [true, '']
+  }
+});
+
+const Semster = mongoose.model('Semesters', semsterSchema);
+
+module.exports = Semster;
