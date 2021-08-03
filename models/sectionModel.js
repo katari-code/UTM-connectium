f7303dad5ac7550e0e1fd2e9369426dba1fb0fd2
Ibrahim Katari <elsaid@live.utm.my>
2021-08-03 18:20:53 +0200

    adding the maming

--- models/sectionModel.js
+++ models/sectionModel.js
@@ -1,58 +1,37 @@
 const mongoose = require('mongoose');
 const sectionSchema = new mongoose.Schema({
   instructorId: {
-    // type: mongoose.Schema.ObjectId,
-    // ref: 'users',
-    type: String,
+    type: mongoose.Schema.ObjectId,
+    ref: 'users',
     required: [true, ''],
-
-    // required: [true, '']
     default: ''
   },
   courseId: {
-    // type: mongoose.Schema.ObjectId,
-    // ref: 'courses',
-    type: String,
+    type: mongoose.Schema.ObjectId,
+    ref: 'courses',
     required: [true, ''],
-
     default: ''
-    // required: [true, '']
   },
   departmentCode: {
     type: String,
     required: [true, ''],
-
     default: ''
-    // required: [true, '']
   },
   semsterId: {
-    // type: mongoose.Schema.ObjectId,
-    // required: [true, ''],
-    // ref: 'semesters',
-    // required: [true, '']
-    type: String,
-    type: String,
+    type: mongoose.Schema.ObjectId,
+    ref: 'semesters',
     default: ''
   },
   students: [
     {
-      // type: mongoose.Schema.ObjectId,
-      // ref: 'users',
       type: String,
       default: '[]'
     }
   ],
-
-  // status: {
-  //   type: String,
-  //   enum: ['Open for registration', 'Full', 'Closed'],
-  //   default: 'Open for registration'
-  // },
   capacity: {
     type: Number,
     required: [true, '']
   },
-
   date: [
     {
       type: Object,
